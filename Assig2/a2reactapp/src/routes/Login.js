e01c63d60caa57e696d016428e006d7aa4e8c48f
trany056 <trany056@mymail.unisa.edu.au>
2024-10-24 17:50:30 +1030

    Add the login banner and logo and change the background color for the entire page.

--- Assig2/a2reactapp/src/routes/Login.js
+++ Assig2/a2reactapp/src/routes/Login.js
@@ -1,15 +1,18 @@
 ﻿import css from './Login.css';
+import logo from '../logo.png';
+import loginBanner from '../assets/login-banner.jpg';
 
 function Login() {
     return (
         <div className="container mt-5 mb-5">
             <div className="row">
                 <div className="col-6 d-flex justify-content-center align-items-center bg-white shadow-sm border border-end-0 border-secondary login-image-container">
-                    <img src="extra_images/login_photo.jpeg" className="login-image" />
+                    <img src={loginBanner} className="login-image" />
                 </div>
 
                 <div className="col-6 d-flex justify-content-center align-items-center bg-white shadow-sm border border-start-0 border-secondary">
                     <div className="text-center">
+                        <img src={logo} alt="logo" style={{ width: "23%" }} className="mb-3" />
                         <h2 className="fw-bold login-font">WELCOME BACK,</h2>
                         <p className="text-muted login-font">Please login to continue to use the services.</p>
 
