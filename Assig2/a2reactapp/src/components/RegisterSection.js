c5fd53f0d39b0219c11d59f87f98973cec11376e
trany056 <trany056@mymail.unisa.edu.au>
2024-10-24 23:37:06 +1030

    Implement the Register function.

--- Assig2/a2reactapp/src/components/RegisterSection.js
+++ Assig2/a2reactapp/src/components/RegisterSection.js
@@ -1,22 +1,47 @@
 ﻿import '../routes/Register.css';
+import { useEffect, useState } from 'react';
+import sha256 from 'crypto-js/sha256';
 
 function RegisterSection() {
+    const [registerStatus, setStatus] = useState(false);
+    const [userName, setUserName] = useState('');
+    const [userPassword, setPassword] = useState('');
+
+    useEffect(() => {
+        fetch(`http://localhost:5147/api/Register?userName=${userName}&passwordHash=${userPassword}`)
+            .then(response => { return response.json() })
+            .then(data => setStatus(data))
+            .catch(err => { console.log(err) });
+    }, [userName, userPassword]);
+
+    if (registerStatus === true) {
+        window.location.href = '/Dashboard';
+    }
+
+    function searchUserNameAndPassword() {
+        const userNameValue = document.querySelector('[name="username"]').value;
+        const passwordValue = document.querySelector('[name="password"]').value;
+
+        setUserName(userNameValue);
+        setPassword(sha256(passwordValue));
+    }
+
+    function onSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <form action="/Register" method="post" className="text-start mt-4">
+        <form action="/Register" onSubmit={onSubmit} method="post" className="text-start mt-4">
             <div className="mb-3">
                 <label htmlFor="username" className="form-label fw-bold login-font">Username</label>
                 <input type="text" name="username" placeholder="Enter your desired username" className="form-control login-font" required />
             </div>
             <div className="mb-3">
                 <label htmlFor="password" className="form-label fw-bold login-font">Password</label>
-                <input type="password" name="password" placeholder="Create a password" className="form-control login-font" required />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="passwordReEntered" className="form-label fw-bold login-font">Re-enter your password</label>
-                <input type="password" name="passwordReEntered" placeholder="Re-enter the password you've just provided" className="form-control login-font" required />
+                <input type="password" name="password" placeholder="Create a new password" maxLength="64" className="form-control login-font" required />
             </div>
             <div className="d-grid mx-auto col-12 mt-4">
-                <button type="submit" className="btn btn-dark login-font">Submit Details</button>
+                <button type="submit" className="btn btn-dark login-font" onClick={searchUserNameAndPassword}>Submit Details</button>
             </div>
         </form>
     );
