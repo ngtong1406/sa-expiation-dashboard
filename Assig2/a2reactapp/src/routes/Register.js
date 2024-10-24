00a5bb86e297d9bf392b6ceb6340d7a6d6e115e1
trany056 <trany056@mymail.unisa.edu.au>
2024-10-24 21:05:00 +1030

    Create a view for Register and its layout.

---
+++ Assig2/a2reactapp/src/routes/Register.js
@@ -0,0 +1,21 @@
+﻿import './Register.css';
+
+function Register() {
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="d-flex justify-content-center align-items-center bg-white shadow-sm border border-start-0 border-secondary">
+                <div className="text-center">
+                    <img src={logo} alt="logo" style={{ width: "23%" }} className="mb-3" />
+                    <h2 className="fw-bold login-font">WELCOME BACK,</h2>
+                    <p className="text-muted login-font">Please login to continue to use the services.</p>
+
+                    <div className="d-grid mx-auto col-12 mt-3">
+                        <a href="#" className="btn btn-outline-dark login-font">Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
