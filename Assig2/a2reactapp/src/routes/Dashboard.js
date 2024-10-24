8d1b4e644e961fd934279517f5cdcdf4be7dcd0b
trany056 <trany056@mymail.unisa.edu.au>
2024-10-25 00:21:04 +1030

    Create a skeleton dropdown list in the Dashboard view.

--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,1 +1,13 @@
-﻿
+﻿function Dashboard() {
+    return (
+        <div className="container mt-4 mb-4">
+            <div className="d-flex justify-content-center align-items-center">
+                <select id="suburbList" className="form-control border border-secondary-subtle shadow-sm" style={{ width: "25%" }}>
+                    <option value="" selected>All Suburbs...</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
