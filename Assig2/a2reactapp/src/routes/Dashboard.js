3d4fee62e2a084fe9ffd8995ed1649fa78e2b2fa
trany056 <trany056@mymail.unisa.edu.au>
2024-10-25 00:29:05 +1030

    Implement code that fetches the suburb list and display the records as a dropdown list.

--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,9 +1,23 @@
-﻿function Dashboard() {
+﻿import { useEffect, useState } from 'react';
+
+function Dashboard() {
+    const [suburbList, setList] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5147/api/Get_ListCameraSuburbs")
+            .then(response => { return response.json() })
+            .then(data => setList(data))
+            .catch(err => { console.log(err) });
+    }, []);
+
     return (
         <div className="container mt-4 mb-4">
             <div className="d-flex justify-content-center align-items-center">
                 <select id="suburbList" className="form-control border border-secondary-subtle shadow-sm" style={{ width: "25%" }}>
                     <option value="" selected>All Suburbs...</option>
+                    {suburbList.map(suburb => (
+                        <option value={suburb}>{suburb}</option>
+                    ))};
                 </select>
             </div>
         </div>
