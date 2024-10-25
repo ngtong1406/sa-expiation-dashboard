d9f25dd2b7dbb0bfa1d183b96c93002735355a90
trany056 <trany056@mymail.unisa.edu.au>
2024-10-25 14:30:47 +1030

    Create a new React component and move the suburb dropdown into it.

---
+++ Assig2/a2reactapp/src/components/SuburbDropdown.js
@@ -0,0 +1,23 @@
+﻿import { useEffect, useState } from 'react';
+
+function SuburbDropdown() {
+    const [suburbList, setList] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5147/api/Get_ListCameraSuburbs")
+            .then(response => { return response.json() })
+            .then(data => setList(data))
+            .catch(err => { console.log(err) });
+    }, []);
+
+    return (
+        <select id="suburbList" className="form-control border border-secondary-subtle shadow-sm" style={{ width: "25%" }}>
+            <option value="" selected>All Suburbs...</option>
+            {suburbList.map(suburb => (
+                <option value={suburb}>{suburb}</option>
+            ))};
+        </select>
+    );
+}
+
+export default SuburbDropdown;
--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,24 +1,11 @@
-﻿import { useEffect, useState } from 'react';
+﻿import SuburbDropdown from "../components/SuburbDropdown";
 
 function Dashboard() {
-    const [suburbList, setList] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:5147/api/Get_ListCameraSuburbs")
-            .then(response => { return response.json() })
-            .then(data => setList(data))
-            .catch(err => { console.log(err) });
-    }, []);
 
     return (
         <div className="container mt-4 mb-4">
             <div className="d-flex justify-content-center align-items-center">
-                <select id="suburbList" className="form-control border border-secondary-subtle shadow-sm" style={{ width: "25%" }}>
-                    <option value="" selected>All Suburbs...</option>
-                    {suburbList.map(suburb => (
-                        <option value={suburb}>{suburb}</option>
-                    ))};
-                </select>
+                <SuburbDropdown />
             </div>
         </div>
     );
