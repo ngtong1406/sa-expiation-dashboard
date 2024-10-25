e7dde31fa1914ef195271375c4e5c9e4e18763fc
trany056 <trany056@mymail.unisa.edu.au>
2024-10-25 14:56:29 +1030

    Display the OffenceSearch content in the Dashboard

--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,8 +1,10 @@
-﻿function Dashboard() {
+﻿import OffenceSearch from "../components/OffenceSearch";
+
+function Dashboard() {
 
     return (
         <div className="container mt-4 mb-4">
-            
+            <OffenceSearch />
         </div>
     );
 }
