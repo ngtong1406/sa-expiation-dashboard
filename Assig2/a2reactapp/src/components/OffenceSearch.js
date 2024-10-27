47ff7918aa99cd5c8eade257f70dc447a178133f
trany056 <trany056@mymail.unisa.edu.au>
2024-10-28 01:29:21 +1030

    Move the fetch request into a useEffect() function and add extra conditions to return appropriate data.

--- Assig2/a2reactapp/src/components/OffenceSearch.js
+++ Assig2/a2reactapp/src/components/OffenceSearch.js
@@ -1,20 +1,32 @@
-﻿import { useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 import SuburbDropdown from "../components/SuburbDropdown";
 
 function OffenceSearch() {
     const [cameraList, setList] = useState([]);
+    const [suburb, setSuburb] = useState('');
+
+    useEffect(() => {
+        if (suburb != "" || suburb != null) {
+            fetch(`http://localhost:5147/api/Get_ListCamerasInSuburb?suburb=${suburb}&cameraIdsOnly=false`)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        return []
+                    }
+                })
+                .then(data => setList(data))
+                .catch(err => { console.log(err) });
+        }
+    }, [suburb]);
 
     function onSubmit(e) {
         e.preventDefault();
 
         const form = e.target;
         const formData = new FormData(form);
-        const suburb = formData.get('suburb');
-
-        fetch(`http://localhost:5147/api/Get_ListCamerasInSuburb?suburb=${suburb}&cameraIdsOnly=false`)
-            .then(response => { return response.json() })
-            .then(data => setList(data))
-            .catch(err => { console.log(err) });
+        let suburb = formData.get('suburb');
+        setSuburb(suburb);
     }
 
     return (
@@ -24,32 +36,36 @@ function OffenceSearch() {
                 <button type="submit" className="btn btn-primary">Search</button>
             </form>
 
-            <div className="table-responsive mt-3">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Location ID</th>
-                            <th>Camera Type Code</th>
-                            <th>Camera Type Name</th>
-                            <th>Suburb</th>
-                            <th>Road Name</th>
-                            <th>Road Type</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cameraList.map(camera => (
+            {cameraList.length == 0 ? (
+                <h1>Nothing just yet.</h1>
+            ) : (
+                <div className="table-responsive mt-3">
+                    <table className="table">
+                        <thead>
                             <tr>
-                                <td>{camera.locationId}</td>
-                                <td>{camera.cameraTypeCode}</td>
-                                <td>{camera.cameraType1}</td>
-                                <td>{camera.suburb}</td>
-                                <td>{camera.roadName}</td>
-                                <td>{camera.roadType}</td>
+                                <th>Location ID</th>
+                                <th>Camera Type Code</th>
+                                <th>Camera Type Name</th>
+                                <th>Suburb</th>
+                                <th>Road Name</th>
+                                <th>Road Type</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            {cameraList.map(camera => (
+                                    <tr key={camera.locationId}>
+                                    <td>{camera.locationId}</td>
+                                    <td>{camera.cameraTypeCode}</td>
+                                    <td>{camera.cameraType1}</td>
+                                    <td>{camera.suburb}</td>
+                                    <td>{camera.roadName}</td>
+                                    <td>{camera.roadType}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
     );
 }
