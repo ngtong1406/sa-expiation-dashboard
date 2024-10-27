7a4d5357cd0d5bc0347d8c0ab374c2f322513391
trany056 <trany056@mymail.unisa.edu.au>
2024-10-28 03:42:40 +1030

    Add labels to the date picker inputs and modify their appearance.

--- Assig2/a2reactapp/src/components/ExpiationDatePicker.js
+++ Assig2/a2reactapp/src/components/ExpiationDatePicker.js
@@ -1,8 +1,11 @@
 ﻿export default function ExpiationDatePicker() {
     return (
-        <div className="d-flex justify-content-center gap-3">
-            <input type="date" name="startDate" title="Date from..." className="form-control border border-secondary shadow-sm" />
-            <input type="date" name="endDate" title="To date..." className="form-control border border-secondary shadow-sm" />
+        <div className="d-flex justify-content-center align-items-bottom gap-3">
+            <label for="startDate" className="form-label">From...</label>
+            <input type="date" name="startDate" title="Date from..." className="form-control border border-secondary-subtle shadow-sm" />
+
+            <label for="endDate" className="form-label">To...</label>
+            <input type="date" name="endDate" title="Date to..." className="form-control border border-secondary-subtle shadow-sm" />
         </div>
     )
 }
