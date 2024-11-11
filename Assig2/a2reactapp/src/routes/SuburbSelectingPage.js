fbfc82f1a89b31c11faade18baf564244bfce23c
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 16:57:53 +1030

    Add an event listener to the suburb dropdown to store value every time the value is changed.

--- Assig2/a2reactapp/src/routes/SuburbSelectingPage.js
+++ Assig2/a2reactapp/src/routes/SuburbSelectingPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function SuburbSelectingPage() {
     const [suburbList, setList] = useState([]);
+    const [chosenSuburb, setSuburb] = useState('');
 
     useEffect(() => {
         fetch("http://localhost:5147/api/Get_ListCameraSuburbs")
@@ -12,6 +13,11 @@ export default function SuburbSelectingPage() {
             .catch(err => { console.log(err) });
     }, []);
 
+    function onChange(e) {
+        let suburb = e.target.value;
+        setSuburb(suburb);
+    }
+
     return (
         <>
             <div className="bg-white justify-content-center rounded border border-secondary-subtle p-3">
@@ -19,7 +25,7 @@ export default function SuburbSelectingPage() {
                 <p>Welcome to the SAPOL's MPDC location search tool, powered by a range of advanced filters that allow you to find the best and the latest locations to install mobile cameras!</p>
                 <p>First thing first, let's start by choosing a suburb below:</p>
                 <div className="d-flex justify-content-center align-middle gap-3">
-                    <select name="suburb" className="form-control border border-secondary-subtle shadow-sm">
+                    <select name="suburb" className="form-control border border-secondary-subtle shadow-sm" onChange={onChange}>
                         <option value="">All Suburbs...</option>
                         {suburbList.map(suburb => (
                             <option key={suburb} value={suburb}>{suburb}</option>
