04ba0a863607afaa20c577653ed1540d0f4a078f
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 15:33:41 +1030

    Include a <datalist> tag and assign it to the searchbox input

--- Assig2/a2reactapp/src/components/DescriptionSearchbox.js
+++ Assig2/a2reactapp/src/components/DescriptionSearchbox.js
@@ -10,6 +10,9 @@ export default function DescriptionSearch() {
     }, [])
 
     return (
-        <input name="descriptionSearch" placeholder="Search for an offence description..." autoComplete="off" type="text" className="form-control border border-secondary-subtle shadow-sm" />
+        <>
+            <input name="descriptionSearch" placeholder="Search for an offence description..." autoComplete="off" type="text" className="form-control border border-secondary-subtle shadow-sm" list="offenceSuggestions" />
+            <datalist id="offenceSuggestions"></datalist>
+        </>
     )
 }
