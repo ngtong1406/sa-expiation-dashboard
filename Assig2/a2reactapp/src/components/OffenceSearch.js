2ac770842492f800fff0afcb94a62682a46b5359
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 16:24:28 +1030

    Remove and comment out the testing pieces of code.

--- Assig2/a2reactapp/src/components/OffenceSearch.js
+++ Assig2/a2reactapp/src/components/OffenceSearch.js
@@ -9,25 +9,6 @@ function OffenceSearch() {
     const [suburb, setSuburb] = useState('');
     const [offenceDescription, setDescription] = useState('');
 
-    /*
-    The code below is just an example table that serves testing purposes and is meant to show what the 
-    final table will look like and how it will behave in Part B when the searching filters are fully functional.
-    */
-    useEffect(() => {
-        if (suburb != "" || suburb != null) {
-            fetch(`http://localhost:5147/api/Get_ListCamerasInSuburb?suburb=${suburb}&cameraIdsOnly=false`)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        return []
-                    }
-                })
-                .then(data => setList(data))
-                .catch(err => { console.log(err) });
-        }
-    }, [suburb]);
-
     function onSubmit(e) {
         e.preventDefault();
 
@@ -85,42 +66,42 @@ function OffenceSearch() {
                 <button type="submit" className="btn btn-primary">Search</button>
             </form>
 
-            {/*
-            The code below is just an example table that serves testing purposes and is meant to show what the 
-            final table will look like and how it will behave in Part B when the searching filters are fully functional.
-            */}
-            {cameraList.length == 0 ? (
-                <div className="text-center text-muted mt-4">
-                    Nothing here yet. Try searching with a description or choosing one of the filters above.
-                </div>
-            ) : (
-                <div className="table-responsive bg-white rounded border border-secondary-subtle shadow-sm mt-3 p-3">
-                    <table className="table table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th>Location ID</th>
-                                <th>Type Code</th>
-                                <th>Type Name</th>
-                                <th>Suburb</th>
-                                <th>Road Name</th>
-                                <th>Road Type</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cameraList.map(camera => (
-                                <tr key={[camera.locationId, camera.suburb]}>
-                                    <td>{camera.locationId}</td>
-                                    <td>{camera.cameraTypeCode}</td>
-                                    <td>{camera.cameraType1}</td>
-                                    <td>{camera.suburb}</td>
-                                    <td>{camera.roadName}</td>
-                                    <td>{camera.roadType}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
+            {/**/}{/*
+            */}{/*The code below is just an example table that serves testing purposes and is meant to show what the */}{/*
+            */}{/*final table will look like and how it will behave in Part B when the searching filters are fully functional.*/}{/*
+            */}{/**/}
+            {/*{cameraList.length == 0 ? (*/}
+            {/*    <div className="text-center text-muted mt-4">*/}
+            {/*        Nothing here yet. Try searching with a description or choosing one of the filters above.*/}
+            {/*    </div>*/}
+            {/*) : (*/}
+            {/*    <div className="table-responsive bg-white rounded border border-secondary-subtle shadow-sm mt-3 p-3">*/}
+            {/*        <table className="table table-hover table-striped">*/}
+            {/*            <thead>*/}
+            {/*                <tr>*/}
+            {/*                    <th>Location ID</th>*/}
+            {/*                    <th>Type Code</th>*/}
+            {/*                    <th>Type Name</th>*/}
+            {/*                    <th>Suburb</th>*/}
+            {/*                    <th>Road Name</th>*/}
+            {/*                    <th>Road Type</th>*/}
+            {/*                </tr>*/}
+            {/*            </thead>*/}
+            {/*            <tbody>*/}
+            {/*                {cameraList.map(camera => (*/}
+            {/*                    <tr key={[camera.locationId, camera.suburb]}>*/}
+            {/*                        <td>{camera.locationId}</td>*/}
+            {/*                        <td>{camera.cameraTypeCode}</td>*/}
+            {/*                        <td>{camera.cameraType1}</td>*/}
+            {/*                        <td>{camera.suburb}</td>*/}
+            {/*                        <td>{camera.roadName}</td>*/}
+            {/*                        <td>{camera.roadType}</td>*/}
+            {/*                    </tr>*/}
+            {/*                ))}*/}
+            {/*            </tbody>*/}
+            {/*        </table>*/}
+            {/*    </div>*/}
+            {/*)}*/}
         </div>
     );
 }
