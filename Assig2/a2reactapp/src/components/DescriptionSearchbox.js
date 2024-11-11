18f43fde575102e3bbb088768200abab63e5deb2
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 15:34:47 +1030

    Create a const value to store the fetched offence list

--- Assig2/a2reactapp/src/components/DescriptionSearchbox.js
+++ Assig2/a2reactapp/src/components/DescriptionSearchbox.js
@@ -2,10 +2,13 @@
 
 export default function DescriptionSearch() {
 
+    // Store all the fetched offences to be used in the auto-complete suggestionn box.
+    const [offenceList, setOffenceList] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:5147/api/Get_SearchOffencesByDescription?offenceCodesOnly=false")
             .then(response => { return response.json() })
-            .then(data => data)
+            .then(data => setOffenceList(data))
             .catch(err => { console.log("Could not retrieve the offence list in the searchbox: " + err) })
     }, [])
 
