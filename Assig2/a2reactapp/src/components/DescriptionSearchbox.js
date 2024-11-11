5fef8045435bde1a2f6ef4b4ce5d6b02aa0cc25b
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 15:38:03 +1030

    Map the fetched offence list and create <option> elements in the <datalist> tag.

--- Assig2/a2reactapp/src/components/DescriptionSearchbox.js
+++ Assig2/a2reactapp/src/components/DescriptionSearchbox.js
@@ -15,7 +15,13 @@ export default function DescriptionSearch() {
     return (
         <>
             <input name="descriptionSearch" placeholder="Search for an offence description..." autoComplete="off" type="text" className="form-control border border-secondary-subtle shadow-sm" list="offenceSuggestions" />
-            <datalist id="offenceSuggestions"></datalist>
+            {offenceList.length > 0 ? 
+                <datalist id="offenceSuggestions">
+                    {offenceList.map((offence) => (
+                        <option value={offence.description}>{offence.description}</option>
+                    ))}
+                </datalist>
+            : <></>}
         </>
     )
 }
