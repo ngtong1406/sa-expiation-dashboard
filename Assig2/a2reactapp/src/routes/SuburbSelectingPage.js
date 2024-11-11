f7dcbec53e6fd8ca33ded7df8b3d80c87618fae0
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 17:13:50 +1030

    Update the layout for the suburb-selecting page and edit the destination route of the Link component

--- Assig2/a2reactapp/src/routes/SuburbSelectingPage.js
+++ Assig2/a2reactapp/src/routes/SuburbSelectingPage.js
@@ -1,5 +1,4 @@
-﻿import SuburbDropdown from '../components/SuburbDropdown';
-import { useEffect, useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SuburbSelectingPage() {
@@ -19,21 +18,23 @@ export default function SuburbSelectingPage() {
     }
 
     return (
-        <>
-            <div className="bg-white justify-content-center rounded border border-secondary-subtle p-3">
-                <h2 className="fw-bold">Welcome!</h2>
-                <p>Welcome to the SAPOL's MPDC location search tool, powered by a range of advanced filters that allow you to find the best and the latest locations to install mobile cameras!</p>
-                <p>First thing first, let's start by choosing a suburb below:</p>
-                <div className="d-flex justify-content-center align-middle gap-3">
-                    <select name="suburb" className="form-control border border-secondary-subtle shadow-sm" onChange={onChange}>
-                        <option value="">All Suburbs...</option>
-                        {suburbList.map(suburb => (
-                            <option key={suburb} value={suburb}>{suburb}</option>
-                        ))};
-                    </select>
-                    <Link to={"/Dashboard/" + chosenSuburb} className="btn btn-primary">Start searching</Link>
+        <div className="container mt-5 mb-5">
+            <div className="row row-cols-1 justify-content-center">
+                <div className="col-7 bg-white rounded border border-secondary-subtle shadow-sm p-5">
+                    <h2 className="fw-bold text-center">Welcome!</h2>
+                    <p className="text-center">Welcome to the SAPOL's MPDC location search tool, powered by a range of advanced filters that allow you to find the best and the latest locations to install mobile cameras!</p>
+                    <p className="text-center mt-5">First thing first, let's start by choosing a suburb below:</p>
+                    <div className="d-flex justify-content-center align-middle gap-3">
+                        <select name="suburb" className="form-control border border-secondary-subtle shadow-sm" onChange={onChange} style={{ width: '35%' }}>
+                            <option value="">All Suburbs...</option>
+                            {suburbList.map(suburb => (
+                                <option key={suburb} value={suburb}>{suburb}</option>
+                            ))};
+                        </select>
+                        <Link to={"/Main/Dashboard/" + chosenSuburb} className="btn btn-dark">Start searching</Link>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
