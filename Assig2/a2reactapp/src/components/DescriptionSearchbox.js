8f3f8eafb5dbf468829fc8ce4817b34166c1f066
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 15:32:21 +1030

    Add a fetch code to obtain all the offences to be used for the auto-complete suggestions.

--- Assig2/a2reactapp/src/components/DescriptionSearchbox.js
+++ Assig2/a2reactapp/src/components/DescriptionSearchbox.js
@@ -1,4 +1,14 @@
-﻿export default function DescriptionSearch() {
+﻿import { useEffect, useState } from 'react';
+
+export default function DescriptionSearch() {
+
+    useEffect(() => {
+        fetch("http://localhost:5147/api/Get_SearchOffencesByDescription?offenceCodesOnly=false")
+            .then(response => { return response.json() })
+            .then(data => data)
+            .catch(err => { console.log("Could not retrieve the offence list in the searchbox: " + err) })
+    }, [])
+
     return (
         <input name="descriptionSearch" placeholder="Search for an offence description..." autoComplete="off" type="text" className="form-control border border-secondary-subtle shadow-sm" />
     )
