65201c75bb4e67acc4f9d3a13c04189d29f48b80
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 15:42:51 +1030

    Rename the attributes to make sure it suits the ReactJS syntax

--- Assig2/a2reactapp/src/components/ExpiationDatePicker.js
+++ Assig2/a2reactapp/src/components/ExpiationDatePicker.js
@@ -1,10 +1,10 @@
 ﻿export default function ExpiationDatePicker() {
     return (
         <div className="d-flex justify-content-center align-items-bottom gap-3">
-            <label for="startDate" className="form-label">From...</label>
+            <label htmlFor="startDate" className="form-label">From...</label>
             <input type="date" name="startDate" title="Date from..." className="form-control border border-secondary-subtle shadow-sm" />
 
-            <label for="endDate" className="form-label">To...</label>
+            <label htmlFor="endDate" className="form-label">To...</label>
             <input type="date" name="endDate" title="Date to..." className="form-control border border-secondary-subtle shadow-sm" />
         </div>
     )
--- Assig2/a2reactapp/src/components/TypeCodeRadio.js
+++ Assig2/a2reactapp/src/components/TypeCodeRadio.js
@@ -19,35 +19,35 @@
 
     return (
         <div id="typeCodeRadio" className="d-flex gap-3 bg-white rounded border border-secondary-subtle shadow-sm p-2 px-4">
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code1" value="F" onClick={revealButton} />
-                <label class="form-check-label" for="code1">F</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code1" value="F" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code1">F</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code2" value="I/section" onClick={revealButton} />
-                <label class="form-check-label" for="code2">I/section</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code2" value="I/section" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code2">I/section</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code3" value="M" onClick={revealButton} />
-                <label class="form-check-label" for="code3">M</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code3" value="M" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code3">M</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code4" value="Mid Block" onClick={revealButton} />
-                <label class="form-check-label" for="code4">Mid Block</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code4" value="Mid Block" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code4">Mid Block</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code5" value="P2P" onClick={revealButton} />
-                <label class="form-check-label" for="code5">P2P</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code5" value="P2P" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code5">P2P</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code6" value="PAC" onClick={revealButton} />
-                <label class="form-check-label" for="code6">PAC</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code6" value="PAC" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code6">PAC</label>
             </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="typeCode" id="code7" value="Rail" onClick={revealButton} />
-                <label class="form-check-label" for="code7">Rail</label>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="typeCode" id="code7" value="Rail" onClick={revealButton} />
+                <label className="form-check-label" htmlFor="code7">Rail</label>
             </div>
-            <a id="clearChoice" onClick={clearChoice} class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#" style={{ display: 'none' }}>
+            <a id="clearChoice" onClick={clearChoice} className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#" style={{ display: 'none' }}>
                 Clear choice
             </a>
         </div>
