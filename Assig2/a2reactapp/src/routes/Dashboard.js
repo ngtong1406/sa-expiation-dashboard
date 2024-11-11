5d9906143c8d5bbfe4f0cb81b48fa62358936bf1
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 17:59:40 +1030

    Modify the Dashboard component so that it obtains the suburb value from the URL parameters and passes it to the search.

--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,10 +1,13 @@
 ﻿import OffenceSearch from "../components/OffenceSearch";
+import { useParams } from 'react-router-dom'
 
 function Dashboard() {
+    let params = useParams();
+    let suburb = params.suburb;
 
     return (
         <div className="container mt-4 mb-4">
-            <OffenceSearch />
+            <OffenceSearch suburb={suburb} />
         </div>
     );
 }
