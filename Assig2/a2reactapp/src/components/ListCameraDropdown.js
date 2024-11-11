8bea79e6e44b602e100bfcae71473de3a49d728e
trany056 <trany056@mymail.unisa.edu.au>
2024-11-11 16:19:24 +1030

    Add a const variable to store a list of fetched camera locations and a fetch to populate that list.

--- Assig2/a2reactapp/src/components/ListCameraDropdown.js
+++ Assig2/a2reactapp/src/components/ListCameraDropdown.js
@@ -1,5 +1,19 @@
-﻿export default function ListCameraDropdown({ suburb }) {
+﻿import { useEffect, useState } from 'react';
+
+// This component will accept 'suburb' as parameter to show the appropriate list of locations.
+export default function ListCameraDropdown({ suburb }) {
+    const [cameraLocationList, setLocationList] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5147/api/Get_ListCamerasInSuburb?suburb=${suburb}&cameraIdsOnly=false`)
+            .then(response => { return response.json() })
+            .then(data => setLocationList(data))
+            .catch(err => { console.log("Could not retrieve the camera locations in the dropdown: " + err) })
+    }, [suburb])
+
     return (
-        <></>
+        <>
+            
+        </>
     )
 }
