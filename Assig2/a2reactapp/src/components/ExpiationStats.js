599713b91c2f9824ea0eb8125bd186e3d886ece4
trany056 <trany056@mymail.unisa.edu.au>
2024-11-13 14:43:37 +1030

    Modify the sub-headings' bootstrap styling.

--- Assig2/a2reactapp/src/components/ExpiationStats.js
+++ Assig2/a2reactapp/src/components/ExpiationStats.js
@@ -4,7 +4,7 @@ export default function ExpiationStats({ expiationStats }) {
     return (
         <div id="statsContainer" className="mt-4 mb-4">
             <h3 className="text-center fw-bold mb-3">Highlights</h3>
-            <p className="text-center">The cards below describe the key highlights about the current search criteria.</p>
+            <p className="text-center text-muted">The cards below describe the key highlights about the current search criteria.</p>
 
             <div className="row mt-4">
                 <div className="col-4">
@@ -72,7 +72,7 @@ export default function ExpiationStats({ expiationStats }) {
             </div>
 
             <h3 className="text-center fw-bold mt-4 mb-3">Insights</h3>
-            <p className="text-center">The chart below describes the total number of offences (<strong>{expiationStats.totalOffencesCount}</strong>) distributed over the weekdays.</p>
+            <p className="text-center text-muted">The chart below describes the total number of offences (<strong>{expiationStats.totalOffencesCount}</strong>) distributed over the weekdays.</p>
 
             <div className="row">
                 <div className="col-12">
