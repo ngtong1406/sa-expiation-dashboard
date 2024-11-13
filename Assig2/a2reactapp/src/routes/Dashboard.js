56b7171cb5b9b076758250dce280beccd98baa13
trany056 <trany056@mymail.unisa.edu.au>
2024-11-13 16:52:01 +1030

    Add a fetch call for suburb list to display appropriate content based on the incoming suburb value.

--- Assig2/a2reactapp/src/routes/Dashboard.js
+++ Assig2/a2reactapp/src/routes/Dashboard.js
@@ -1,13 +1,30 @@
 ﻿import OffenceSearch from "../components/OffenceSearch";
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 function Dashboard() {
     let params = useParams();
     let suburb = params.suburb;
 
+    const [suburbList, setList] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5147/api/Get_ListCameraSuburbs')
+            .then(response => { return response.json() })
+            .then(data => setList(data))
+            .catch(err => { console.log("An error occurred when attempting to retrieve suburb list: " + err) })
+    }, [suburb])
+
     return (
         <div className="container mt-4 mb-4">
-            <OffenceSearch suburb={suburb} />
+            {suburbList.some(s => s === suburb) ? <>
+                <OffenceSearch suburb={suburb} />
+            </> : <>
+                <div className="text-center justify-content-center">
+                    <div className="alert alert-danger text-center mt-4 mb-4">Sorry! Could not find any suburb provided. Please go back and select a suburb.</div>
+                    <Link to="/Main" className="btn btn-dark text-center">Go back</Link>
+                </div>
+            </>}
         </div>
     );
 }
